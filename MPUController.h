69577641dce4bf1b33ee66006d91ce5c76aaf0f6
neumaier <sebastian.neumaier@wu.ac.at>
2019-01-24 20:55:01 +0100

    MPU controller

---
+++ MPUController.h
@@ -0,0 +1,18 @@
+#include "MPU6050.h"
+
+class MPUController {
+
+public:
+  void initialize();
+  void getMotion6(int *ax, int *ay, int *az, int *gx, int *gy, int *gz);
+};
+
+
+void MPUController::initialize() {
+
+}
+
+
+void MPUController::getMotion6(int *ax, int *ay, int *az, int *gx, int *gy, int *gz) {
+
+}
